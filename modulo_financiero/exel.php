70c28c3f8e1f4f2064e90d68efa02d987d49ed9a
MaterBit <julecci@gmail.com>
2013-04-05 14:21:57 -0500

    g

--- modulo_financiero/exel.php
+++ modulo_financiero/exel.php
@@ -8,13 +8,13 @@ header("Content-Disposition:  filename=\"movimientos.xls\";")?>
       <td height='30' align='center'><table width='700' border='0' align='center' cellpadding='0' cellspacing='2'><tr>
           <td width='780' height='60' align='left' valign='middle' bgcolor='#E6CCCD' class='texto_azul' ><table border='0' cellpadding='0' cellspacing='3'>
             <tr>
-              <td colspan='4'><span class='texto_azul2'><strong>&nbsp;Movimiento 03</strong></span><strong> - </strong>04/04/2013</td>
+              <td colspan='4'><span class='texto_azul2'><strong>&nbsp;Movimiento 03</strong></span><strong> - </strong>05/04/2013</td>
             </tr>
             <tr>
               <td colspan='4'><img src='images/line2.gif' width='760' height='1' /></td>
             </tr>
             <tr>
-              <td colspan='4'><strong>&nbsp;Nombre de tercero:</strong>1019039622&nbsp;&nbsp;&nbsp;&nbsp;<strong>Nit:</strong>JORGE&nbsp;&nbsp;&nbsp;&nbsp;<strong>Concepto:</strong>jj</td>
+              <td colspan='4'><strong>&nbsp;Nombre de tercero:</strong>1019039622&nbsp;&nbsp;&nbsp;&nbsp;<strong>Nit:</strong>JORGE&nbsp;&nbsp;&nbsp;&nbsp;<strong>Concepto:</strong>fff</td>
             </tr>
           </table></td>
           <td width='164' align='left' valign='middle' bgcolor='#D9B0B3' class='texto_azul'> &nbsp;&nbsp;<span class='texto_azul_peque'>Documento</span>:CXC 2</td>
@@ -29,45 +29,8 @@ header("Content-Disposition:  filename=\"movimientos.xls\";")?>
             </tr><tr class='tr_tabla_interna'>
                   <td width='74' class='td_tabla_interna' align='center' >1</td>
                   <td class='td_tabla_interna' align='center' >ACTIVO</td>
-                  <td width='108' class='td_tabla_interna' align='center' >100</td>
+                  <td width='108' class='td_tabla_interna' align='center' >20000</td>
                   <td width='113' class='td_tabla_interna' align='center' >0</td>
-                </tr><tr class='tr_tabla_interna'>
-                  <td width='74' class='td_tabla_interna' align='center' >11</td>
-                  <td class='td_tabla_interna' align='center' >DISPONIBLE</td>
-                  <td width='108' class='td_tabla_interna' align='center' >0</td>
-                  <td width='113' class='td_tabla_interna' align='center' >5</td>
-                </tr></table></td>
-          </tr><tr>
-          <td width='780' height='60' align='left' valign='middle' bgcolor='#E6CCCD' class='texto_azul' ><table border='0' cellpadding='0' cellspacing='3'>
-            <tr>
-              <td colspan='4'><span class='texto_azul2'><strong>&nbsp;Movimiento 03</strong></span><strong> - </strong>04/04/2013</td>
-            </tr>
-            <tr>
-              <td colspan='4'><img src='images/line2.gif' width='760' height='1' /></td>
-            </tr>
-            <tr>
-              <td colspan='4'><strong>&nbsp;Nombre de tercero:</strong>1019039622&nbsp;&nbsp;&nbsp;&nbsp;<strong>Nit:</strong>JORGE&nbsp;&nbsp;&nbsp;&nbsp;<strong>Concepto:</strong>jj</td>
-            </tr>
-          </table></td>
-          <td width='164' align='left' valign='middle' bgcolor='#D9B0B3' class='texto_azul'> &nbsp;&nbsp;<span class='texto_azul_peque'>Documento</span>:CXC 2</td>
-          </tr>
-        <tr>
-          <td height='35' colspan='6' valign='top'><table width='760' border='0' align='center' cellpadding='0' cellspacing='2'  align='center'>
-            <tr>
-              <td height='20' class='td_resaltado_azul' align='center' >Cuenta</td>
-              <td width='595' class='td_resaltado_azul' align='center' >Descripción</td>
-              <td class='td_resaltado_azul' align='center' ><div align='center'>Débito</div></td>
-              <td class='td_resaltado_azul' align='center' ><div align='center'>Crédito</div></td>
-            </tr><tr class='tr_tabla_interna'>
-                  <td width='74' class='td_tabla_interna' align='center' >1</td>
-                  <td class='td_tabla_interna' align='center' >ACTIVO</td>
-                  <td width='108' class='td_tabla_interna' align='center' >100</td>
-                  <td width='113' class='td_tabla_interna' align='center' >0</td>
-                </tr><tr class='tr_tabla_interna'>
-                  <td width='74' class='td_tabla_interna' align='center' >11</td>
-                  <td class='td_tabla_interna' align='center' >DISPONIBLE</td>
-                  <td width='108' class='td_tabla_interna' align='center' >0</td>
-                  <td width='113' class='td_tabla_interna' align='center' >5</td>
                 </tr></table></td>
           </tr><tr>
           <td width='780' height='60' align='left' valign='middle' bgcolor='#E6CCCD' class='texto_azul' ><table border='0' cellpadding='0' cellspacing='3'>
