d2e509aae9eeaeb48200fdb722e83a82b5fa4caf
jorge <jorgedipra@gmail.com>
2013-04-08 17:48:06 -0500

    migracion a nuevo diseño
    hay q arreglarlo sirve provicional mente solo con una cuenta

--- modulo_financiero/agregar_comprobante_diario.php
+++ modulo_financiero/agregar_comprobante_diario.php
@@ -14,45 +14,43 @@ $tercero_lista = array();
 foreach ($tercero as $item) {
 	$tercero_lista[] = utf8_encode($item->getNombretercero()); 
 }
-
-
 $DocumentoDAO = new DocumentoDAO();
 $documentos = new documentos();
 $documento = $DocumentoDAO->getList();
-
-
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="es">
 <head>
+<meta charset="utf-8" /> 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="Page-Enter" content="blendtrans(duration=1)">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta name="viewport" content="width = device-width, initial-scale=1, maximum-scale=1"/>
+<meta name="description" content="software de propiedad horizontal">
+<meta property="og:title" content="Cassius" />
+<meta property="og:type" content="software" />
+<meta property="og:url" content=""/>
+<meta property="og:image" content="" />
+<meta property="og:site_name" content="Cassius" />
 <title>Cassius - software de propiedad horizontal</title>
 <link href="config/estilos_cassius.css" rel="stylesheet" type="text/css" />
-
-
 <script src="Scripts/bloqueo_clic_derecho.js" type="text/javascript"></script>
 <script type="text/javascript" src="Scripts/codigo.js"></script>
-
-
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type='text/javascript' src='js/jquery.bgiframe.min.js'></script>
 <script type='text/javascript' src='js/jquery.autocomplete.js'></script>
 <link rel="stylesheet" type="text/css" href="js/jquery.autocomplete.css" />
-
 <link rel="stylesheet" href="css/datepicker.css" type="text/css" />
 <script type="text/javascript" src="js/datepicker.js"></script>
 <script type="text/javascript" src="js/eye.js"></script>
 <script type="text/javascript" src="js/utils.js"></script>
 <script type="text/javascript" src="js/layout.js?ver=1.0.2"></script>
-
 <script type="text/javascript">
 function MM_preloadImages() { //v3.0
   var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
     var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
     if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
 }
-
 function MM_openBrWindow(theURL,winName,features) { //v2.0
   ventana=window.open(theURL,winName,features);
   alto=screen.height;
@@ -61,7 +59,6 @@ function MM_openBrWindow(theURL,winName,features) { //v2.0
   xposi=(ancho-970)/2;
   ventana.moveTo(xposi,yposi);
 }
-
 function dato_tercero(){
 	var tercero = document.getElementById('tercero').value;
 	
@@ -69,12 +66,10 @@ function dato_tercero(){
 	llamarasincrono('tercero_campo2.php?tercero='+tercero, 'ter2');
 	
 }
-
 function dato_tercero2(){
 	llamarasincrono('tercero_campo.php', 'ter');
 	llamarasincrono('tercero_campo2.php', 'ter2');
 }
-
 function numero_documento(){
 	
 	var documento = document.getElementById('documento').value;
@@ -92,27 +87,20 @@ function numero_documento(){
 		mostar_item();
 	}
 }
-
 function mostar_item(){
 	alert('Por favor ingrese a continuacion los valores de las cuentas.');
      llamarasincrono('agregar_item.php', 'agregar_i');
 }
-
-
-
 function datos_tercero(id,nombre){
 	document.getElementById('identifica').value = id;
 	document.getElementById('tercero').value = nombre;
 	document.getElementById('concepto').focus();
 	llamarasincrono('tercero_campo.php?nit='+id, 'ter');
 }
-
-
 function buscar_cuenta(id){
 	var idcuenta = document.getElementById('idcuenta'+id).value;
 	llamarasincrono('cuenta_documento.php?idcuenta='+idcuenta+'&n='+id, 'cu'+id);
 }
-
 function valida_cuenta(id){
 	var idcuenta = document.getElementById('idcuenta'+id).value;
 	var cuenta = document.getElementById('cuenta'+id).value;
@@ -127,8 +115,6 @@ function valida_cuenta(id){
 		document.getElementById('idcuenta'+id).focus();
 	}	
 }
-
-
 function valida_debito(id){
 	var debito = document.getElementById('debito'+id).value;
 	if(debito.length > 0){
@@ -137,7 +123,6 @@ function valida_debito(id){
 		 document.getElementById('credito'+id).disabled = "";
 	}
 }
-
 function valida_credito(id){
 	var credito = document.getElementById('credito'+id).value;
 	if(credito.length > 0){
@@ -146,8 +131,6 @@ function valida_credito(id){
 		 document.getElementById('debito'+id).disabled = "";
 	}
 }
-
-
 function sumar_cuentas_d(id){
 	var debito = document.getElementById('debito'+id).value;
 	var id_suma_debito = document.getElementById('id_suma_debito').value;
@@ -160,10 +143,7 @@ function sumar_cuentas_d(id){
 		document.getElementById('suma_debito').value = debito;	
 	}
 
-}
-
-
-	
+}	
 function validar(){
 	var identifica = document.getElementById('identifica').value;
 	if(identifica == ""){
@@ -186,49 +166,36 @@ function validar(){
 		return false;
 	}
 
-}
-	
+}	
 function agregar_item(num){
 	var num2 =  num + 1;
 	llamarasincrono('agregar_item.php?n='+num2, 'agregar_i');
 	llamarasincrono('campo_cuenta_movi.php?n='+num2, 'compo_cuenta_m'+num2);
-}
-	
+}	
 function eliminar_cuenta(id){
 	llamarasincrono('campo_cuenta_movi.php?id='+id, 'compo_cuenta_m'+id);
-}
-		
-	
+}	
 function agregar_Tercero(){
 	MM_openBrWindow('agregar_tercero2.php?OK=1&nombre='+document.getElementById('tercero').value,'AgregarTercero','scrollbars=yes,width=970px,height=400px')
 }
-
-
 function cerrarVentana(){ 
 	alert('Movimiento creado con exito.');
 	 window.opener.location.href = window.opener.location.href; 
 	//window.opener.document.forms[0].submit();
-
-
-
-  if (window.opener.progressWindow) 
-		
+  if (window.opener.progressWindow) 		
  { 
 	//var id = 1;
     window.opener.progressWindow.close()
   } 
-  
   window.close(); 
 
 }
-
 function cerrar_v(){ 
 	alert('Movimiento creado con exito.');
 	 window.opener.location.href = window.opener.location.href; 
 	//window.opener.document.forms[0].submit();
 	
-  if (window.opener.progressWindow) 
-		
+  if (window.opener.progressWindow) 		
  { 
 	//var id = 1;
     window.opener.progressWindow.close()
--- modulo_financiero/contabilidad/movimientos/agregar_componentes_diario.php
+++ modulo_financiero/contabilidad/movimientos/agregar_componentes_diario.php
@@ -1,8 +1,8 @@
 <form id="form1" name="form1" method="post" action="php/action/addMovimiento.php" onsubmit="return validar()">
-
 <table width="945" border="0" align="center" cellpadding="0" cellspacing="0">
   <tr>
-    <td width="422" height="50" align="center" valign="middle" bgcolor="#ADB1CB"><table width="500" border="0" cellspacing="0" cellpadding="0">
+    <td width="422" height="50" align="center" valign="middle" bgcolor="#ADB1CB">
+      <table width="500" border="0" cellspacing="0" cellpadding="0">
       <tr>
         <td width="256" class="titulos" align="center">Agregar Comprobante de diario</td>
       </tr>
@@ -12,52 +12,56 @@
     <td height="20" valign="middle" align="center">&nbsp;</td>
   </tr>
   <tr>
-    <td height="35" valign="middle" align="center"><table width="950" border="0" align="center" cellpadding="0" cellspacing="2">
+    <td height="35" valign="middle" align="center">
+      <table width="950" border="0" align="center" cellpadding="0" cellspacing="2">
       <tr>
-        <td width="713" height="90" align="left" valign="middle" bgcolor="#E6CCCD" class="texto_azul"><table width="720" border="0" cellpadding="0" cellspacing="3">
-          <tr>
-            <td width="331"><span class="texto_azul2"><strong>&nbsp;Movimiento <input name="num_movi" id="num_movi" type="hidden" value="<?php if(count($movimiento) == 0){ echo count($movimiento) + 1; }else{ echo count($movimiento) + 1; }  ?>" /> <?php if(count($movimiento) == 0){ echo count($movimiento) + 1; }else{ echo count($movimiento) + 1; }  ?></strong></span><strong> - </strong>
-             <input type="text" name="fecha" class="inputDate textarea_redondo2" id="inputDate" style="width:65px;" value="<?php echo date('d/m/Y'); ?>" readonly="readonly" />
-               </td>
-            <td width="326" valign="middle"><strong>&nbsp;Nombre de tercero:</strong>&nbsp;<input name="tercero" type="text" class="textarea_redondo2" id="tercero" style="width:180px;" onblur="javascript:dato_tercero()"/><div id="ter"></div></td>
-            <td width="51" valign="middle"><div id="ter2"></div></td>
-          </tr>
-          <tr>
-            <td colspan="3"><img src="images/line2.gif" width="700" height="1" /></td>
-          </tr>
-          <tr>
-            <td colspan="3"><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Concepto:</strong><span class="td_tabla_interna">
-              <input name="concepto" type="text" class="textarea_redondo2" id="concepto" style="width:422px;"/>
-              </span></td>
-          </tr>
-        </table></td>
-        <td align="left" valign="middle" bgcolor="#D9B0B3" class="texto_azul">
-        
-        &nbsp;
-        <table width="220" border="0">
+        <td width="215" height="90" align="left" valign="middle" bgcolor="#D9B0B3" class="texto_azul">
+          <table width="220" border="0">
           <tr>
             <td width="166">&nbsp;<span class="texto_azul_peque">Documento</span>:
              <select name="documento" class="textarea_redondo2" id="documento" style="width:80px; height:27px;" onchange="numero_documento();">
             <option value="0">--</option>
             <?php foreach($documento as $item){ ?>
-          		<option value="<?php echo $item->getId(); ?>">
+              <option value="<?php echo $item->getId(); ?>">
               <?php echo $item->getNombredoc(); ?></option>
             <?php } ?>
             </select></td>
             <td width="55"><div id="num_d"></div></td>
           </tr>
-        </table>        
-                   
-            
-            
+        </table>  
+        </td>
+
+        <td width="729" align="left" valign="middle" bgcolor="#E6CCCD" class="texto_azul">
+          <table width="705" border="0" cellpadding="0" cellspacing="3">
+          <tr>
+            <td>
+              <span class="texto_azul2"><strong>&nbsp;Movimiento 
+              <input name="num_movi" id="num_movi" type="hidden" value="<?php if(count($movimiento) == 0){ echo count($movimiento) + 1; }else{ echo count($movimiento) + 1; }  ?>" /> <?php if(count($movimiento) == 0){ echo count($movimiento) + 1; }else{ echo count($movimiento) + 1; }  ?></strong></span><strong> - </strong>
+             <input type="text" name="fecha" class="inputDate textarea_redondo2" id="inputDate" style="width:65px;" value="<?php echo date('d/m/Y'); ?>" readonly="readonly" />
+
+              <strong>&nbsp;Nombre de tercero:</strong>&nbsp;
+              <input name="tercero" type="text" class="textarea_redondo2" id="tercero"
+               style="width:180px;" onblur="javascript:dato_tercero()"/>               
+              <td valign="middle"><div id="ter2"></div></td>
+              <center><div id="ter"></div></center>
             </td>
-        </tr>
-      <tr>
-        <td height="35" colspan="2" valign="top"><div id="cuentas"></div></td>
+          </tr>
+          <tr>
+            <td><img src="images/line2.gif" width="700" height="1" /></td>
+          </tr>
+          <tr>
+            <td colspan="3">
+              <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Concepto:</strong>
+              <span class="td_tabla_interna">
+              <input name="concepto" type="text" class="textarea_redondo2" id="concepto" style="width:422px;"/>
+              </span>
+            </td>
+          </tr>
+        </table></td>
       </tr>
-      
-      
-      
+      <tr>    
+          <td height="35" colspan="2" valign="top"><div id="cuentas"></div></td>
+      </tr>
     </table></td>
   </tr>
   <tr>
@@ -69,9 +73,7 @@
     <input type="hidden" name="num_movimiento" id="num_movimiento" value="<?php if(count($movimiento) == 0){ echo count($movimiento) + 1; }else{ echo count($movimiento); }  ?>" />
     <input type="hidden" name="suma_debito" id="suma_debito" value="0" />
     <input type="hidden" name="id_suma_debito" id="id_suma_debito" value="0" />
-    
-</td>
-
+    </td>
   </tr>
 </table>
 </form>
