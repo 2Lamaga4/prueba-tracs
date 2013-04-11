6e39a6f59d82fe8f542b302cd15925c4457a89c9
jorge <jorgedipra@gmail.com>
2013-04-10 19:37:29 -0500

    julian me quemo

--- consultas.sql
+++ consultas.sql
@@ -1,8 +1,12 @@
 select Sigla,nodocumento,nombretercero from terceros inner join identificacion;
+select Sigla,nodocumento,nombretercero from terceros inner join identificacion where tipodocumento = IdTipoidentificacion && estado = 1;
 
 SELECT * FROM  identificacion;
 
 ALTER TABLE identificacion ADD Sigla VARCHAR(5);
+
+ALTER TABLE terceros ADD Estado VARCHAR(5);
+
 
 1 conexion
 2 clase que la maneja
