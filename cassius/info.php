e030766cf750d7a606702bd4aba4d874f8a33218
jorge <jorgedipra@gmail.com>
2013-04-16 08:55:57 -0500

    logo e icono

--- cassius/info.php
+++ cassius/info.php
@@ -7,9 +7,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 <meta name="viewport" content="width = device-width, initial-scale=1, maximum-scale=1"/>
 <meta name="description" content="software de propiedad horizontal">
-<meta property="og:title" content="Cassius" />
+<meta property="og:title" content="Cassius - software de propiedad horizontal" />
 <meta property="og:type" content="software" />
-<meta property="og:url" content=""/>
-<meta property="og:image" content="" />
+<meta property="og:url" content="Cassius.com"/>
+<meta property="og:image" content="logo.JPG" />
 <meta property="og:site_name" content="Cassius" />
-<title>Cassius - software de propiedad horizontal</title>
+<title>Cassius</title>
+<link rel="shortcut icon" href="favicon.ico" />
