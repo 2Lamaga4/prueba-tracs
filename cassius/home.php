c93aa591c1d7927569a22609d61ca8998ee97d87
jorge <jorgedipra@gmail.com>
2013-04-15 23:01:54 -0500

    movimiento de directorios

--- cassius/home.php
+++ cassius/home.php
@@ -1,15 +1,15 @@
 <?php
  include_once ('info.php');//se llama la informacion de la pagina
 ?>
-<link href="ModuloAdministrativo/Contabilidad/config/estilos_cassius.css" rel="stylesheet" type="text/css" />
-<script src="ModuloAdministrativo/Contabilidad/Scripts/swfobject_modified.js"></script>
-<script src="ModuloAdministrativo/Contabilidad/Scripts/bloqueo_clic_derecho.js"></script>
-<script src="ModuloAdministrativo/Contabilidad/Scripts/globos_ayuda.js"></script>
+<link href="Modulo/Contabilidad/config/estilos_cassius.css" rel="stylesheet" type="text/css" />
+<script src="Modulo/Contabilidad/Scripts/swfobject_modified.js"></script>
+<script src="Modulo/Contabilidad/Scripts/bloqueo_clic_derecho.js"></script>
+<script src="Modulo/Contabilidad/Scripts/globos_ayuda.js"></script>
 </head>
 <body class="home" OnContextMenu="return false">
 <div id="logo">
   <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="590" height="215" id="FlashID" title="logo">
-    <param name="movie" value="ModuloAdministrativo/Contabilidad/images/logo.swf" />
+    <param name="movie" value="Modulo/Contabilidad/images/logo.swf" />
     <param name="quality" value="high" />
     <param name="wmode" value="transparent" />
     <param name="swfversion" value="11.0.0.0" />
@@ -21,7 +21,7 @@
     <!-- La siguiente etiqueta object es para navegadores distintos de IE.
      Ocúltela a IE mediante IECC. -->
     <!--[if !IE]>-->
-    <object type="application/x-shockwave-flash" data="ModuloAdministrativo/Contabilidad/images/logo.swf" width="590" height="215">
+    <object type="application/x-shockwave-flash" data="Modulo/Contabilidad/images/logo.swf" width="590" height="215">
       <!--<![endif]-->
       <param name="quality" value="high" />
       <param name="wmode" value="transparent" />
@@ -62,7 +62,7 @@
     </tr>
     <tr>
       <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      
-        <input style="width:90px;"name="Entrar" type="button" class="boton_redondo" id="Entrar" value="::: Entrar :::" onclick="location.href='ModuloAdministrativo/menu_home.php'"/></td>
+        <input style="width:90px;"name="Entrar" type="button" class="boton_redondo" id="Entrar" value="::: Entrar :::" onclick="location.href='Modulo/menu_home.php'"/></td>
     </tr>
   </table>
 </div>
