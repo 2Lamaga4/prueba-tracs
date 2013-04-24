80bea34592827c4140eaeb54f91d210e3c41b2f1
lamaga <2la.ma.ga4@gmail.com>
2013-04-24 15:55:31 -0500

    EXCEL.

--- cassius/Modulo/Contabilidad/RegistroContable/exel.php
+++ cassius/Modulo/Contabilidad/RegistroContable/exel.php
@@ -8,6 +8,92 @@ header("Content-Disposition:  filename=\"movimientos.xls\";")?>
       <td height='30' align='center'><table width='700' border='0' align='center' cellpadding='0' cellspacing='2'><tr>
           <td width='780' height='60' align='left' valign='middle' bgcolor='#E6CCCD' class='texto_azul' ><table border='0' cellpadding='0' cellspacing='3'>
             <tr>
+              <td colspan='4'><span class='texto_azul2'><strong>&nbsp;Movimiento 09</strong></span><strong> - </strong>24/04/2013</td>
+            </tr>
+            <tr>
+              <td colspan='4'><img src='../images/line2.gif' width='760' height='1' /></td>
+            </tr>
+            <tr>
+              <td colspan='4'><strong>&nbsp;Nombre de tercero:</strong>79689582&nbsp;&nbsp;&nbsp;&nbsp;<strong>Nit:</strong>william Bueno Hernandez&nbsp;&nbsp;&nbsp;&nbsp;<strong>Concepto:</strong>ww</td>
+            </tr>
+          </table></td>
+          <td width='164' align='left' valign='middle' bgcolor='#D9B0B3' class='texto_azul'> &nbsp;&nbsp;<span class='texto_azul_peque'>Documento</span>:CD 2</td>
+          </tr>
+        <tr>
+          <td height='35' colspan='6' valign='top'><table width='760' border='0' align='center' cellpadding='0' cellspacing='2'  align='center'>
+            <tr>
+              <td height='20' class='td_resaltado_azul' align='center' >Cuenta</td>
+              <td width='595' class='td_resaltado_azul' align='center' >Descripción</td>
+              <td class='td_resaltado_azul' align='center' ><div align='center'>Débito</div></td>
+              <td class='td_resaltado_azul' align='center' ><div align='center'>Crédito</div></td>
+            </tr></table></td>
+          </tr><tr>
+          <td width='780' height='60' align='left' valign='middle' bgcolor='#E6CCCD' class='texto_azul' ><table border='0' cellpadding='0' cellspacing='3'>
+            <tr>
+              <td colspan='4'><span class='texto_azul2'><strong>&nbsp;Movimiento 08</strong></span><strong> - </strong>24/04/2013</td>
+            </tr>
+            <tr>
+              <td colspan='4'><img src='../images/line2.gif' width='760' height='1' /></td>
+            </tr>
+            <tr>
+              <td colspan='4'><strong>&nbsp;Nombre de tercero:</strong>78592&nbsp;&nbsp;&nbsp;&nbsp;<strong>Nit:</strong>william &nbsp;&nbsp;&nbsp;&nbsp;<strong>Concepto:</strong>hgfd</td>
+            </tr>
+          </table></td>
+          <td width='164' align='left' valign='middle' bgcolor='#D9B0B3' class='texto_azul'> &nbsp;&nbsp;<span class='texto_azul_peque'>Documento</span>:CXC 7</td>
+          </tr>
+        <tr>
+          <td height='35' colspan='6' valign='top'><table width='760' border='0' align='center' cellpadding='0' cellspacing='2'  align='center'>
+            <tr>
+              <td height='20' class='td_resaltado_azul' align='center' >Cuenta</td>
+              <td width='595' class='td_resaltado_azul' align='center' >Descripción</td>
+              <td class='td_resaltado_azul' align='center' ><div align='center'>Débito</div></td>
+              <td class='td_resaltado_azul' align='center' ><div align='center'>Crédito</div></td>
+            </tr><tr class='tr_tabla_interna'>
+                  <td width='74' class='td_tabla_interna' align='center' >11200501</td>
+                  <td class='td_tabla_interna' align='center' >Banco de Bogota Cta xxxxxxxxxx</td>
+                  <td width='108' class='td_tabla_interna' align='center' >5</td>
+                  <td width='113' class='td_tabla_interna' align='center' >0</td>
+                </tr><tr class='tr_tabla_interna'>
+                  <td width='74' class='td_tabla_interna' align='center' >13050505</td>
+                  <td class='td_tabla_interna' align='center' >Cuotas de Administracion</td>
+                  <td width='108' class='td_tabla_interna' align='center' >0</td>
+                  <td width='113' class='td_tabla_interna' align='center' >5</td>
+                </tr></table></td>
+          </tr><tr>
+          <td width='780' height='60' align='left' valign='middle' bgcolor='#E6CCCD' class='texto_azul' ><table border='0' cellpadding='0' cellspacing='3'>
+            <tr>
+              <td colspan='4'><span class='texto_azul2'><strong>&nbsp;Movimiento 07</strong></span><strong> - </strong>23/04/2013</td>
+            </tr>
+            <tr>
+              <td colspan='4'><img src='../images/line2.gif' width='760' height='1' /></td>
+            </tr>
+            <tr>
+              <td colspan='4'><strong>&nbsp;Nombre de tercero:</strong>1019039622&nbsp;&nbsp;&nbsp;&nbsp;<strong>Nit:</strong>JORGE&nbsp;&nbsp;&nbsp;&nbsp;<strong>Concepto:</strong>d</td>
+            </tr>
+          </table></td>
+          <td width='164' align='left' valign='middle' bgcolor='#D9B0B3' class='texto_azul'> &nbsp;&nbsp;<span class='texto_azul_peque'>Documento</span>:CXC 6</td>
+          </tr>
+        <tr>
+          <td height='35' colspan='6' valign='top'><table width='760' border='0' align='center' cellpadding='0' cellspacing='2'  align='center'>
+            <tr>
+              <td height='20' class='td_resaltado_azul' align='center' >Cuenta</td>
+              <td width='595' class='td_resaltado_azul' align='center' >Descripción</td>
+              <td class='td_resaltado_azul' align='center' ><div align='center'>Débito</div></td>
+              <td class='td_resaltado_azul' align='center' ><div align='center'>Crédito</div></td>
+            </tr><tr class='tr_tabla_interna'>
+                  <td width='74' class='td_tabla_interna' align='center' >11100501</td>
+                  <td class='td_tabla_interna' align='center' >Banco de Bogota cta xxxxxxxxxx</td>
+                  <td width='108' class='td_tabla_interna' align='center' >555</td>
+                  <td width='113' class='td_tabla_interna' align='center' >0</td>
+                </tr><tr class='tr_tabla_interna'>
+                  <td width='74' class='td_tabla_interna' align='center' >13050505</td>
+                  <td class='td_tabla_interna' align='center' >Cuotas de Administracion</td>
+                  <td width='108' class='td_tabla_interna' align='center' >0</td>
+                  <td width='113' class='td_tabla_interna' align='center' >234</td>
+                </tr></table></td>
+          </tr><tr>
+          <td width='780' height='60' align='left' valign='middle' bgcolor='#E6CCCD' class='texto_azul' ><table border='0' cellpadding='0' cellspacing='3'>
+            <tr>
               <td colspan='4'><span class='texto_azul2'><strong>&nbsp;Movimiento 06</strong></span><strong> - </strong>19/04/2013</td>
             </tr>
             <tr>
